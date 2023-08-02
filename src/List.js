7351a201e9230d0494cfa211c5a137b71f19fb17
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-08-02 16:56:31 +0200

    small change in list.js, test commit

--- src/List.js
+++ src/List.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const List = ([people]) => {
-  return (
-    <>
-    {people.map((person)=>{
-      
-    })}
-    </>
-  );
+const List = ({ people }) => {
+    return (
+        <>
+            {people.map((person) => {
+                const { id, name, age, image } = person;
+                return (
+                    <>
+                        <article key={id} className="person">
+                            <img src="{img}" alt="" />
+                        </article>
+                    </>
+                );
+            })}
+        </>
+    );
 };
 
 export default List;
